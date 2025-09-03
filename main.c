ba70d5ceeb3f179aa0a5e5a1c5dabbd48276e0b5
angryCar80 <ouabbasaymen@gmail.com>
2025-09-03 21:30:22 +0100

    Made the edit command and its working

--- main.c
+++ main.c
@@ -7,6 +7,12 @@
 #define MAX_TASKS 100
 #define MAX_LEN 100
 
+struct Human {
+  char name[40];
+  bool alive;
+};
+
+// The use of this is for later
 typedef struct {
   char name[MAX_LEN];
   bool toggled;
@@ -16,7 +22,7 @@ void ClearScreen() { printf("\033[2J\033[1;1H"); }
 
 // THE MAIN FUNCTION //
 int main() {
-  char input[100];
+  char input[200];
   char tasks[MAX_TASKS][MAX_LEN];
   int taskCount = 0;
 
@@ -29,10 +35,10 @@ int main() {
 
     if (strcmp(input, "clear") == 0) {
       ClearScreen();
-    } else if (strcmp(input, "exit") == 0) {
+    } else if (strcmp(input, "exit") == 0) { // the exit command
       printf("Exiting...\n");
       break;
-    } else if (strncmp(input, "add ", 4) == 0) {
+    } else if (strncmp(input, "add ", 4) == 0) { // the add command
       if (taskCount < MAX_TASKS) {
         strncpy(tasks[taskCount], input + 4, MAX_LEN - 1);
         tasks[taskCount][MAX_LEN - 1] = '\0'; // FIXED
@@ -41,11 +47,16 @@ int main() {
       } else {
         printf("Task list is full!\n");
       }
-    } else if (strcmp(input, "list") && strcmp(input, "ls") == 0) {
-      printf("\nTasks:\n");
-      for (int i = 0; i < taskCount; i++) {
-        printf("%d. %s", i + 1, tasks[i]); // FIXED
-        printf("\n");
+    } else if (strcmp(input, "list") &&
+               strcmp(input, "ls") == 0) { // the list command
+      if (taskCount == 0) {
+        printf("No tasks yet\n");
+      } else {
+        printf("\nTasks:\n");
+        for (int i = 0; i < taskCount; i++) {
+          printf("%d. %s", i + 1, tasks[i]); // FIXED
+          printf("\n");
+        }
       }
     } else if (strncmp(input, "remove", 4) == 0) {
       int index = atoi(input + 7);
@@ -57,6 +68,25 @@ int main() {
         }
         taskCount--;
         printf("Task %d removed\n", index);
+      }
+    } else if (strncmp(input, "echo ", 5) == 0) { // the echo command
+      printf("%s\n", input + 5);
+    } else if (strncmp(input, "edit ", 5) == 0) { // the edit command
+      char new_task_name[200];
+      char *index_str = input + 5;
+      int index = atoi(index_str);
+
+      if (index < 1 || index > taskCount) {
+        printf("Invalid Task Number");
+      } else {
+        printf("Enter a new Task name: ");
+        fgets(new_task_name, sizeof(new_task_name), stdin);
+        new_task_name[strcspn(new_task_name, "\n") == 0];
+
+        strncpy(tasks[index - 1], new_task_name, MAX_LEN - 1);
+        tasks[index - 1][MAX_LEN - 1] = '\n';
+
+        printf("The Task %d updated to: %s", index, new_task_name);
       }
     } else {
       printf("Unknown command: %s\n", input);
